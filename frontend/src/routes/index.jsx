2af127235b1a4767bfb0cdfe71267a8830aa0c19
Kevan <kevanazzaky2007@email.com>
2025-08-05 12:54:17 +0700

    Rename LoginTemp.jsx to Login.jsx

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
-import Login from "../pages/LoginTemp";
+import Login from "../pages/Login";
 
 const router = createBrowserRouter([
   {
